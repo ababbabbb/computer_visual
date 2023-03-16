7307760e26468d3b25e12524603802f82ebec66b
ababbabbb <17826066203@163.com>
2023-03-17 00:48:32 +0800

    Initial commit

--- Framework/agree/syringe_agree.py
+++ Framework/agree/syringe_agree.py
@@ -2,42 +2,42 @@
 
 class _Syringe:
     @staticmethod
-    def _injection_component(instance_cls, attrs):
+    def _injection_component(attrs):
         
-        return instance_cls
+        return attrs
 
     @staticmethod
-    def _injection_lib(instance_cls, attrs):
+    def _injection_lib(attrs):
 
-        return instance_cls
+        return attrs
     
     @staticmethod
-    def _injection_loader(instance_cls, attrs):
+    def _injection_loader(attrs):
 
-        return instance_cls
+        return attrs
     
     @staticmethod
-    def _injection_logger(instance_cls, attrs):
+    def _injection_logger(attrs):
 
-        return instance_cls
+        return attrs
     
     @staticmethod
-    def _injection_monitor(instance_cls, attrs):
+    def _injection_monitor(attrs):
 
-        return instance_cls
+        return attrs
     
     @staticmethod
-    def _injection_runner(instance_cls, attrs):
+    def _injection_runner(attrs):
 
-        return instance_cls
+        return attrs
     
     @staticmethod
-    def _injection_test(instance_cls, attrs):
+    def _injection_test(attrs):
 
-        return instance_cls
+        return attrs
 
 
-def _injection(name_cls: str, instance_cls, attrs):
+def _injection(name_cls: str, attrs):
     """
     本函数起到的作用为分派，将参数依照name_cls传入到获取的不同静态方法中
     :param name_cls: 类名称
@@ -45,20 +45,22 @@ def _injection(name_cls: str, instance_cls, attrs):
     :return: 元类的实例对象——类
     """
 
-    return getattr(_Syringe, '_injection_' + name_cls.lower())(instance_cls, attrs)
+    return getattr(_Syringe, '_injection_' + name_cls.lower())(attrs)
 
 
 class AgreeMeta(type):
     """
         本元类创建的目的是为了向各大类中注入规则
 
-    """
+    """ 
 
     def __new__(mcs, name, bases, attrs):
         attrs["_instance"] = None
-        return _injection(name, super().__new__(mcs, name, bases, attrs), attrs)
+
+        return super().__new__(mcs, name, bases, _injection(name, attrs))
 
     def __call__(self, *args, **kwargs):
         if self._instance is None:
             self._instance = super().__call__(*args, **kwargs)
+
         return self._instance
