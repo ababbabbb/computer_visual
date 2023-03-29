3310bd9e4c7fdbd4164229560de1bcdf899e786a
ababbabbb <17826066203@163.com>
2023-03-30 00:17:28 +0800

    Initial commit

--- Framework/actuator/runner/proxy_runner.py
+++ Framework/actuator/runner/proxy_runner.py
@@ -4,7 +4,7 @@
 #            该代理后续可作为切面编程为对象创建前后钩子方法——》
 #            runner的外部主要使用方式还是在object、datasource(custom)文件中做切面注解——》
 #            内部则在扮演创建工厂时构建对象与runner的联系，为外部注解提供基础——》
-#            注意：外部使用时，是直接将runner实例化后到外部使用的（类装饰器形式）
+#            注意：外部使用时，是直接将runner实例化后到外部使用的（类装饰器形式）//参考flask上下文
 from Framework.agree import AgreeMeta
 
 
